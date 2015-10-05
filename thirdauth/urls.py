c14dd98e7d6163a0f2e024e8e574412d5db438f5
RishabhJain2018 <jain.rishabh170895@gmail.com>
2015-10-06 00:39:52 +0530

    Login using google

---
+++ thirdauth/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'thirdauth.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url('', include('social.apps.django_app.urls', namespace='social')),
+    url(r'^admin/', include(admin.site.urls)),
+)
